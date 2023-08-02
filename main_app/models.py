b907b3efa14ecd94d25dd34111702404a6556e45
chajiiiii <j.cha1708@gmail.com>
2023-08-02 11:20:28 +0100

    Add functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 class Finch(models.Model):
     breed = models.CharField(max_length=50)
     wingspan = models.CharField(max_length=50)
     colors = models.CharField(max_length=50)
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.views.generic.edit import CreateView
+
 from .models import Finch
 
 # finches = [
@@ -25,3 +27,7 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, "finches/detail.html", { 'finch' : finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = "__all__"
