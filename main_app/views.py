38e5c9c8ead8df0197e606ed5832a97acd16dbaa
chajiiiii <j.cha1708@gmail.com>
2023-08-04 02:55:56 +0100

    Toy assign

--- main_app/views.py
+++ main_app/views.py
@@ -30,8 +30,13 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
 
+    id_list = finch.toys.all().values_list("id")
+
+    toys_finch_deosnt_have = Toy.objects.exclude(id__in=id_list)
+
     feeding_form = FeedingForm()
-    return render(request, "finches/detail.html", { 'finch' : finch, 'feeding_form': feeding_form})
+
+    return render(request, "finches/detail.html", { 'finch' : finch, 'feeding_form': feeding_form, 'toys': toys_finch_deosnt_have})
 
 class FinchCreate(CreateView):
     model = Finch
@@ -72,3 +77,12 @@ class ToyDetail(DetailView):
 class ToyCreate(CreateView):
     model = Toy
     fields = '__all__'
+
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
